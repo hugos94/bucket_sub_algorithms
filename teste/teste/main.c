6e7b0bfdad4a238b32ac8a1bea68901029d81900
hugos94 <hugos94@gmail.com>
2016-06-19 19:09:30 -0300

    Implementacao de todos os 4 algoritmos, porem com estouro de pilha para ser resolvido.

---
+++ teste/teste/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  teste
+//
+//  Created by Hugo Santos Piauilino Neto on 19/06/16.
+//  Copyright © 2016 Hugo Santos Piauilino Neto. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
